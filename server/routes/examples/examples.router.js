dcf0e863b3b8dc45cfa06e1d72de464d0a2d6c2b
John <john.alan.blaine@gmail.com>
2021-01-05 12:44:02 -0600

    Add example POST route, stub out PUT/DELETE

--- server/routes/examples/examples.router.js
+++ server/routes/examples/examples.router.js
@@ -5,11 +5,25 @@ import examples from '../../mocks/examples/gizmos.example.json'
 const router = express.Router();
 
 router.get('/gizmos', (req, res) => {
-    res.status(200).send(examples.gizmos);
+  res.status(200).send(examples.gizmos);
 });
 
 router.get('/gizmo', (req, res) => {
-    res.status(200).send(examples.gizmos[0]);
+  res.status(200).send(examples.gizmos[0]);
 });
+
+router.post('/gizmo', (req, res) => {
+  examples.gizmos.push(req.body.data);
+  res.status(201).send(examples.gizmos);
+});
+
+router.put('/gizmo', (req, res) => {
+
+});
+
+router.delete('/gizmo', (req, res) => {
+
+});
+
 
 export default router;
