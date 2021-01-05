bc300c7be0f65b8e0a496128f7f73ba2026b15a1
John <john.alan.blaine@gmail.com>
2021-01-05 14:53:47 -0600

    Complete draft implementation of gizo put/delete

--- server/routes/examples/examples.router.js
+++ server/routes/examples/examples.router.js
@@ -18,11 +18,16 @@ router.post('/gizmo', (req, res) => {
 });
 
 router.put('/gizmo', (req, res) => {
-
+  const gizmoToUpdate = examples.gizmos.find(el => el.name === req.body.data.name);
+  gizmoToUpdate.name = req.body?.data?.name;
+  gizmoToUpdate.type = req.body?.data?.type;
+  res.status(200).send(examples.gizmos);
 });
 
 router.delete('/gizmo', (req, res) => {
-
+  const deleteI = examples.gizmos.findIndex(el => el.name === req.body?.data?.name)
+  const deletedGizmo = examples.gizmos.splice(deleteI, 1);
+  res.status(200).send(deletedGizmo);
 });
 
 
