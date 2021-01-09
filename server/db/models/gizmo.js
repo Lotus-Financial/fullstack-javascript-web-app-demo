cd821cfd4c8296a7dfb4f7502ff7d91950098df4
John <john.alan.blaine@gmail.com>
2021-01-09 12:39:32 -0600

    Set up gizmo sequelize model

--- server/db/models/gizmo.js
+++ server/db/models/gizmo.js
@@ -14,7 +14,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Gizmo.init({
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     type: DataTypes.STRING
   }, {
     sequelize,
