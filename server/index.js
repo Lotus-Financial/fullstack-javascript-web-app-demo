d0bf4234a987274ad042625880a9dbc674185849
John <john.alan.blaine@gmail.com>
2021-01-09 14:39:32 -0600

    Sync database, add db authenticate todo

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const router = require('./routes/router.js');
+const db = require('./db/models');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(router);
+
+// TODO: Decide if force drop and re-sync is needed
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+db.sequelize.sync();
 
 app.listen(3000, () => console.log('Server running on port 3000!'));
 
