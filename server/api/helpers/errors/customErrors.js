79b35f7f6257dd3e69c75bf0c770c91dd1b4e463
John <john.alan.blaine@gmail.com>
2021-01-20 11:46:22 -0600

    Add custom error for bad input

--- server/api/helpers/errors/customErrors.js
+++ server/api/helpers/errors/customErrors.js
@@ -1,3 +1,14 @@
+class InvalidIdError extends Error {
+  constructor(resource, id) {
+    super(`Invalid id ${id} for resource: ${resource}.`)
+
+    this.statusCode = 422;
+    this.status = 'fail';
+    this.name = this.constructor.name;
+
+    Error.captureStackTrace(this,  this.constructor);
+  }
+}
 
 class InvalidRouteError extends Error {
   constructor(method, route) {
@@ -13,18 +24,18 @@ class InvalidRouteError extends Error {
 
 class NotFoundError extends Error {
   constructor(resource, id){
-      super(`${resource} with id ${id} was not found.`);
+      super(`Resource: ${resource} with id ${id} was not found.`);
 
       this.statusCode = 404;
       this.status = 'fail';
       this.name = this.constructor.name;
 
       Error.captureStackTrace(this, this.constructor);
-
   }
 }
 
 module.exports = {
+  InvalidIdError,
   InvalidRouteError,
   NotFoundError
 };
