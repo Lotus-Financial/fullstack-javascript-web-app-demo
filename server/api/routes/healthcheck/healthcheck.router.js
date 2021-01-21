2d45079e8447fa20e640775f009ccffda618746a
John <john.alan.blaine@gmail.com>
2021-01-21 13:05:19 -0600

    Add async handler to healthcheck route

--- server/api/routes/healthcheck/healthcheck.router.js
+++ server/api/routes/healthcheck/healthcheck.router.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const asyncHandler = require('../../helpers/asyncHandler.helper');
+
 const healthcheckGET = (req, res) => {
   res.send('Blue Skies Core is UP!');
 }
 
-router.get('/', healthcheckGET);
+router.get('/', asyncHandler(healthcheckGET));
 
 module.exports = {
   router,
