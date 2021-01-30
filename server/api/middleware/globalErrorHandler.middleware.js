d253fef179a76d4ae8082613a0b1b54c21116848
John <john.alan.blaine@gmail.com>
2021-01-30 14:03:48 -0600

    Obfuscate errors sent to client from globalerrors

--- server/api/middleware/globalErrorHandler.middleware.js
+++ server/api/middleware/globalErrorHandler.middleware.js
@@ -2,8 +2,10 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
 
+  console.error(`Global error handler invoked:\n  Error message: ${err.message}\n  Stacktrace: ${err.stack}`)
+
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    message: 'Internal Server Error'
   });
 }
