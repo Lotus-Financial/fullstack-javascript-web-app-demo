51481d2a7664ffe72cbd083cb9eabf8ae11ab9e1
John <john.alan.blaine@gmail.com>
2021-08-05 17:18:22 -0500

    Set up router to default direct to Home component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
 
 function App() {
   return (
     <div>
-      <header className='App-header'>
-      </header>
+      {/* <header className='App-header'>
+      </header> */}
+      <Switch>
+      <Route exact path ='/' component={Home}/>
+      </Switch>
     </div>
   );
 }
